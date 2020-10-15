0dad36a162ca0a9625ea8471a5214fe5d9353b6f
Gareth McDonald <garethmcdonald1998@hotmail.co.uk>
2020-10-15 18:46:45 +0100

    updates include divide and multiply

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -8,6 +8,14 @@ public class Calculator {
 	public int sub(int a, int b) {
 		return a-b;
 	}
+	public int divide(int a, int b) {
+		return a/b;
+	}
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	
 	
 }
 
